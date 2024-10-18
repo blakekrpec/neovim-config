4824e6ababa0a7c1d8fa550054f8171cd18c6aeb
Blake Krpec <blake.krpec@yahoo.com>
2024-10-18 14:51:41 -0500

    more plugins, and unity lsp

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,4 +7,20 @@ return {
         "MunifTanjim/nui.nvim",
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = true,  -- Hide dotfiles
+                    hide_gitignored = true,  -- Hide files in .gitignore
+                    never_show = { -- List of files or patterns to ignore
+                        ".git",
+                        "*.tmp",
+                        "*.log",
+                        "*.meta", 
+                    },
+                },
+            },
+        })
+    end,
 }
---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,54 @@
+return {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        { "folke/neodev.nvim", opts = {} },
+    },
+    config = function()
+        local nvim_lsp = require("lspconfig")
+        local mason_lspconfig = require("mason-lspconfig")
+
+        local protocol = require("vim.lsp.protocol")
+
+        local on_attach = function(client, bufnr)
+            -- format on save
+            if client.server_capabilities.documentFormattingProvider then
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = vim.api.nvim_create_augroup("Format", { clear = true }),
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format()
+                    end,
+                })
+            end
+        end
+        
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        mason_lspconfig.setup_handlers({
+            function(server)
+                nvim_lsp[server].setup({
+                    capabilities = capabilities,
+                })
+            end,
+            ["clangd"] = function()
+                nvim_lsp["clangd"].setup({
+                    cmd = { "clangd", "--compile-commands-dir=build"},
+                    filetypes = { "c", "cpp", "objc", "objcpp" },
+                    root_dir = require('lspconfig/util').root_pattern("compile_commands.json", ".git"),
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+            ["omnisharp"] = function()
+                nvim_lsp["omnisharp"].setup({
+                    cmd = {"omnisharp"},
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+        })
+    end,
+}
+
