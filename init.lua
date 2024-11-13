8ecb7db28b64921e2eed4d291fc26f4a4a3740b3
Blake Krpec <blake.krpec@yahoo.com>
2024-11-13 10:14:42 -0600

    added auto-session

--- init.lua
+++ init.lua
@@ -2,6 +2,3 @@ require("settings")
 require("maps")
 require("unity").setup() -- must be required before lazy_config
 require("lazy_config")
-
-vim.opt.colorcolumn = "80" -- highlight col 80
-vim.opt.smartindent = true
