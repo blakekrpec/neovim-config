2163da3f47f14d1d6721d3b617a24348cfe3b2ff
Krpec, Blake C <blake.krpec@swri.org>
2025-04-29 14:26:08 -0500

    update lsp formatter to work properly, only tested with cpp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -10,46 +10,54 @@ return {
     config = function()
         local nvim_lsp = require("lspconfig")
         local mason_lspconfig = require("mason-lspconfig")
+        local util = require("lspconfig.util")
 
-        -- Check if a formatting config file exists
-        local function file_exists(filename)
-            return vim.fn.filereadable(filename) == 1
+        -- Search for a config file upward from the buffer path
+        local function config_exists(pattern)
+            local root = util.root_pattern(pattern)(vim.api.nvim_buf_get_name(0))
+            return root ~= nil
         end
 
-        -- Decide if a file type should be formatted
+        -- Determine if formatting should be enabled based on config presence
         local function should_format(ft)
             if ft == "cpp" or ft == "c" then
-                return not file_exists(".clang-format")
+                return config_exists(".clang-format")
             elseif ft == "python" then
-                return not (file_exists("pyproject.toml") and
-                        vim.fn.match(vim.fn.readfile("pyproject.toml"), "tool.black") >= 0)
-                    and not file_exists(".flake8")
+                if config_exists("pyproject.toml") then
+                    local root = util.root_pattern("pyproject.toml")(vim.api.nvim_buf_get_name(0))
+                    local pyproject_path = root .. "/pyproject.toml"
+                    local lines = vim.fn.readfile(pyproject_path)
+                    for _, line in ipairs(lines) do
+                        if line:match("%[tool%.black%]") then
+                            return true
+                        end
+                    end
+                end
+                return config_exists(".flake8")
             elseif ft == "cs" then
-                return not file_exists(".editorconfig")
+                return config_exists(".editorconfig")
             elseif ft == "lua" then
-                return not file_exists(".stylua.toml") or file_exists("stylua.toml")
+                return config_exists(".stylua.toml") or config_exists("stylua.toml")
             end
-            return true
+            return false -- Default to disabling formatting
         end
 
-        -- on_attach function
+        -- on_attach function to handle formatting based on config
         local on_attach = function(client, bufnr)
             if client.server_capabilities.documentFormattingProvider then
-                -- Disable formatting unless explicitly allowed
                 local ft = vim.bo[bufnr].filetype
                 if not should_format(ft) then
-                    print("Disabling LSP formatting for:", ft)
+                    -- print("Disabling LSP formatting for:", ft)
                     client.server_capabilities.documentFormattingProvider = false
                     return
                 end
 
-                -- Enable controlled format-on-save
-                print("Enabling LSP formatting for:", ft)
+                -- print("Enabling LSP formatting for:", ft)
                 vim.api.nvim_create_autocmd("BufWritePre", {
                     group = vim.api.nvim_create_augroup("Format", { clear = true }),
                     buffer = bufnr,
                     callback = function()
-                        print("Running formatter for:", ft)
+                        -- print("Running formatter for:", ft)
                         vim.lsp.buf.format({ async = false })
                     end,
                 })
@@ -69,8 +77,8 @@ return {
             ["clangd"] = function()
                 nvim_lsp["clangd"].setup({
                     cmd = { "clangd" },
-                    filetypes = { "c", "cpp", "objc", "objcpp" },
-                    root_dir = require('lspconfig/util').root_pattern("compile_commands.json", ".git"),
+                    filetypes = { "c", "cpp", "objc", "objcpp", "h", "hpp" },
+                    root_dir = util.root_pattern("compile_commands.json", ".git"),
                     on_attach = on_attach,
                     capabilities = capabilities,
                 })
