05e7bcbbc0810bf9ffb048965322f1c69fbf0423
blakekrpec <blake.krpec@yahoo.com>
2025-04-29 20:37:12 -0500

    add lightbulb, vsnip

---
+++ lua/plugins/nvim-lightbulb.lua
@@ -0,0 +1,9 @@
+return {
+    "kosayoda/nvim-lightbulb",
+    opts = {
+        autocmd = { enabled = true }, -- automatically update the lightbulb
+        sign = { enabled = true },    -- show lightbulb in the sign column
+        virtual_text = { enabled = false }, -- or enable virtual text instead
+    },
+}
+
