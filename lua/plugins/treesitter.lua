5e3cd7c0f528b5d57c2f09de4dcfc8ef2c687ffc
Blake Krpec <blake.krpec@yahoo.com>
2025-05-01 08:07:18 -0500

    added treesitter so that indent-blankline would work

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "python",
+        "cpp",
+        "c_sharp",
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    })
+  end,
+}
