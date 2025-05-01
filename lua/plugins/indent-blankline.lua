43ad56d0f5478c44e49f75805f1171e6d99ace93
Krpec, Blake C <blake.krpec@swri.org>
2025-05-01 08:07:18 -0500

    added treesitter so that indent-blankline would work

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,11 +1,13 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    -- event = { "BufReadPre", "BufNewFile" },
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
-    -- opts = {
-    --   indent = { char = "┊" },
-    -- },
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    indent = { char = "│" },
+    scope = {
+      enabled = true,
+      show_start = true,
+      show_end = false,
+    },
+  },
 }
+
