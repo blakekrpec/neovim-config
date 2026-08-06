9a346fb4a8407daeaf0add70998a118b0aaeb78f
Blake Krpec <blake.krpec@yahoo.com>
2026-08-06 09:51:02 -0500

    fix to respect terminal transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,17 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+        require("rose-pine").setup({
+            disable_background = true,
+            disable_float_background = true,
+        })
         vim.cmd("colorscheme rose-pine")
+        
+        -- Ensure transparency for all background elements
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+        vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+        vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
     end,
 }
